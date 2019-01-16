5c41b870d6951dd303326578eff54f9dff8d460e
palapalaqoo <palapalaqoo@yahoo.com.tw>
2019-01-16 11:27:14 +0800

    190116_1

--- SMI_IdentifyCommand.py
+++ SMI_IdentifyCommand.py
@@ -15,10 +15,12 @@ import sys
 import time
 from time import sleep
 import re
-
+import os
+import csv
+import shutil
 # Import VCT modules
 from lib_vct.NVME import NVME
-from lib_vct.NVME import DevWakeUpAllTheTime
+
 
 class SMI_IdentifyCommand(NVME):
     # Script infomation >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -28,8 +30,10 @@ class SMI_IdentifyCommand(NVME):
     # </Script infomation> <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     
     # <Attributes> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    BuildInFile_CNS01_IdentifyControllerDataStructure = "./lib_vct/CSV/CNS01_IdentifyControllerDataStructure.csv"    
-    UserInFile_CNS01_IdentifyControllerDataStructure = "./CSV/IdentifyFile.csv"  
+    InPath = "./CSV/In/"
+    OutPath = "./CSV/Out/"    
+    File_BuildIn_Identify_CNS01 = "./lib_vct/CSV/CNS01_IdentifyControllerDataStructure.csv"    
+    File_Identify_CNS01 = "Identify_CNS01.csv" 
     
     TypeInt=0x0
     TypeStr=0x1
@@ -39,10 +43,11 @@ class SMI_IdentifyCommand(NVME):
     
     def GetValueFromUserFile(self, UserFile, idName):
         value=None
-        for mItem in UserFile:
-            if self.RemoveSpaces(mItem[0].upper()) == self.RemoveSpaces(idName.upper()):
-                value=mItem[1]
-                break
+        if UserFile!=None:
+            for mItem in UserFile:
+                if self.RemoveSpaces(mItem[0].upper()) == self.RemoveSpaces(idName.upper()):
+                    value=mItem[1]
+                    break
         return value    
     
     def GetIdentifyController(self):
@@ -55,7 +60,7 @@ class SMI_IdentifyCommand(NVME):
         return DS
     
     def GetCliCommandByBuileInFileName(self, BuileInFileName):
-        if BuileInFileName==self.BuildInFile_CNS01_IdentifyControllerDataStructure:
+        if BuileInFileName==self.File_BuildIn_Identify_CNS01:
             CMD = "nvme admin-passthru %s --opcode=0x6 --data-len=4096 -r --cdw10=0x1 2>&1"%self.dev
             
         return CMD     
@@ -68,72 +73,91 @@ class SMI_IdentifyCommand(NVME):
             self.Print( mStr , "f")      
         else:
             self.Print( mStr )      
-    def CheckBuildInWithUserFiles(self, BuileInFileName, UserFileName):
+    def CheckBuildInWithUserFiles(self, BuileInFileName, mUserFileName):
     # BuileInFileName, BuileIn File Name
     # UserFileName,  user file name
+        UserFileNameFullPath=self.InPath+mUserFileName
         subRt=0
-        self.Print("Check if file %s exist or not"%UserFileName)
-        UserFile=self.ReadCSVFile(UserFileName)
+        self.Print("Check if file %s exist or not"%UserFileNameFullPath)
+        UserFile=self.ReadCSVFile(UserFileNameFullPath)
         if UserFile==None:
-            self.Print("Can't find file at %s, skip this test"%SMI_IdentifyCommand.IdentifyFile)
-            return 255
+            self.Print("Can't find file at %s"%UserFileNameFullPath, "w")
         else:  
-            self.Print("File %s exist"%UserFileName)  
+            self.Print("File %s exist"%UserFileNameFullPath)  
             self.Print( "" )    
-            self.Print( "Check if BuileInFile exist or not")
-            BuileInFile = self.ReadCSVFile(BuileInFileName)
-            if BuileInFile==None:
-                self.Print( "BuileInFile is not exist(%s), quit !"%BuileInFileName,"f")
-                return 1
-            else:            
-                self.Print( "BuileInFile exist")
-                self.Print( "Issue identify command to get data structure")
-                
-                CMD = self.GetCliCommandByBuileInFileName(BuileInFileName)
-                # returnd data structure
-                rTDS=self.shell_cmd(CMD)
-                # format data structure to list 
-                DS=self.AdminCMDDataStrucToListOrString(rTDS, 0)            
-                if DS==None:
-                    self.Print( "Fail to get data structure, quit !","f")
-                    return 1
-                else:
-                    self.Print( "Success to get data structure")
-                    self.Print( "Start to check values")
-                    self.Print("")
-                    self.Print("----------------------------------------------------------------------")   
-                    self.PrintAlignString("Name", "Controller", UserFileName)
-                    self.Print("----------------------------------------------------------------------")   
-                    for mItem in BuileInFile:
-                        # if start char=# means it is a comment, and quit it
-                        mStr="^#"
-                        if re.search(mStr, mItem[0]):
-                            pass   
-                        else:                 
-                            Name=mItem[0]                  
-                            StopByte=int(mItem[1])
-                            StartByte=int(mItem[2])                
-                            mType=int(mItem[3])
-                            # value from controller,  and format it
-                            ValueC =self.convert(DS, StopByte, StartByte, mType)
-                            ValueC = self.mFormatString(ValueC, mType)
-                            # value from user,  and format it
-                            ValueU = self.GetValueFromUserFile(UserFile, Name)
-                            ValueU = self.mFormatString(ValueU, mType)
-                                
-                            # check if value is the same from controller and user file    
-                            if ValueU==None:                                
-                                self.PrintAlignString(Name, ValueC, "N/A")
-                            elif ValueU==ValueC:        
-                                self.PrintAlignString(Name, ValueC, ValueU, "pass")                            
-                            elif ValueU!=ValueC:      
-                                self.PrintAlignString(Name, ValueC, ValueU, "fail")   
-                                subRt=1
-                    self.Print("----------------------------------------------------------------------")     
-                    self.Print("Finish")
-                    # end of for mItem in BuileInFile:    
-                    return subRt                                          
 
+        self.Print( "Check if BuileInFile exist or not")
+        BuileInFile = self.ReadCSVFile(BuileInFileName)
+        if BuileInFile==None:
+            self.Print( "BuileInFile is not exist(%s), quit !"%BuileInFileName,"f")
+            return 1
+        else:            
+            self.Print( "BuileInFile exist")
+            self.Print( "Issue identify command to get data structure")
+            
+            CMD = self.GetCliCommandByBuileInFileName(BuileInFileName)
+            # returnd data structure
+            rTDS=self.shell_cmd(CMD)
+            # format data structure to list 
+            DS=self.AdminCMDDataStrucToListOrString(rTDS, 0)            
+            if DS==None:
+                self.Print( "Fail to get data structure, quit !","f")
+                return 1
+            else:
+                self.Print( "Success to get data structure")
+                self.Print( "Start to check values")
+                self.Print("")
+                self.Print("----------------------------------------------------------------------")   
+                self.PrintAlignString("Name", "Controller", UserFileNameFullPath if UserFile!=None else UserFileNameFullPath+"(missing)")
+                self.Print("----------------------------------------------------------------------")   
+                for mItem in BuileInFile:
+                    # if start char=# means it is a comment, and quit it
+                    mStr="^#"
+                    if re.search(mStr, mItem[0]):
+                        pass   
+                    else:                 
+                        Name=mItem[0]                  
+                        StopByte=int(mItem[1])
+                        StartByte=int(mItem[2])                
+                        mType=int(mItem[3])
+                        # value from controller,  and format it
+                        ValueC =self.convert(DS, StopByte, StartByte, mType)
+                        ValueC = self.mFormatString(ValueC, mType)
+                        # value from user,  and format it
+                        ValueU = self.GetValueFromUserFile(UserFile, Name)
+                        ValueU = self.mFormatString(ValueU, mType)
+                            
+                        # check if value is the same from controller and user file    
+                        if ValueU==None:                                
+                            self.PrintAlignString(Name, ValueC, "N/A")
+                        elif ValueU==ValueC:        
+                            self.PrintAlignString(Name, ValueC, ValueU, "pass")                            
+                        elif ValueU!=ValueC:      
+                            self.PrintAlignString(Name, ValueC, ValueU, "fail")   
+                            subRt=1
+                            
+                        # save to csv file
+                        self.SaveToCSVFile(mUserFileName, Name, ValueC)
+                self.Print("----------------------------------------------------------------------")     
+                self.Print("Finish")
+                # end of for mItem in BuileInFile:    
+                return subRt                                          
+
+    def SaveToCSVFile(self, fileName, name, value):
+        fileNameFullPath = self.OutPath+fileName
+        # if file not exist, then create it
+        if not os.path.exists(fileNameFullPath):
+            f = open(fileNameFullPath, "w")
+            f.close()            
+        
+        # write
+        with open(fileNameFullPath, 'a') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([name, value])
+            
+   
+    
+    
     def convert(self, lists, stopByte, startByte, mtype):   
         # return is string , eg, '0x1A' or 'SMI2262'
         # sub bytes
@@ -207,17 +231,36 @@ class SMI_IdentifyCommand(NVME):
         
     def CreateIDFileFromController(self):
         pass     
-                
+    
+    def InitDirs(self):
+        # If ./CSV not exist, Create it
+        if not os.path.exists("./CSV"):
+            os.makedirs("./CSV")
+        
+        # If ./CSV/In not exist, Create it
+        if not os.path.exists(self.InPath):
+            os.makedirs(self.InPath)
+            
+        if not os.path.exists(self.OutPath):
+            os.makedirs(self.OutPath)  
+        
+        # remove all file in /CSV/Out
+        if os.path.exists(self.OutPath):
+            shutil.rmtree(self.OutPath) 
+        # Create dir
+        if not os.path.exists(self.OutPath):
+            os.makedirs(self.OutPath)                        
     
     # </Function> <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     def __init__(self, argv):
         # initial parent class
         super(SMI_IdentifyCommand, self).__init__(argv)
              
+        self.InitDirs()
         
     # override
     def PreTest(self):   
-  
+
         return True
 
             
@@ -228,7 +271,7 @@ class SMI_IdentifyCommand(NVME):
     SubCase1KeyWord = "Synch"
     def SubCase1(self):
         ret_code=0
-        ret_code=self.CheckBuildInWithUserFiles(self.BuildInFile_CNS01_IdentifyControllerDataStructure, self.UserInFile_CNS01_IdentifyControllerDataStructure)
+        ret_code=self.CheckBuildInWithUserFiles(self.File_BuildIn_Identify_CNS01, self.File_Identify_CNS01)
         
         return ret_code
 
