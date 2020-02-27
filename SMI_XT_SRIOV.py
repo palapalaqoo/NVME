8a4003dae089cede66fa24af4212bc08eaa266b7
root <root@localhost>
2020-02-27 17:52:04 +0800

    20200227

--- SMI_XT_SRIOV.py
+++ SMI_XT_SRIOV.py
@@ -3,6 +3,9 @@
 
 # Import python built-ins
 import sys
+from time import sleep
+import re
+
 # Import VCT modules
 from lib_vct.NVME import NVME
 
@@ -21,22 +24,112 @@ class SMI_XT_SRIOV(NVME):
             return int(self.shell_cmd("cat %s"%HMB_filename) )
             
     def TestHMB(self, TargetMHMB):
-        self.Print("Try to reload NVMe driver with max_host_mem_size_mb=%s"%TargetMHMB)
+        HLStr = self.UseStringStyle("max_host_mem_size_mb=%s"%TargetMHMB, fore="red")
+        self.Print("1) Try to reload NVMe driver with %s"%HLStr)
         self.ReloadNVMeDriverWithSpecificParameter("max_host_mem_size_mb=%s"%TargetMHMB)
         CurrentMHMB = self.GetCurrentMaxHMB()
         if CurrentMHMB!=TargetMHMB:
             self.Print("Fail to set max_host_mem_size_mb=%s"%TargetMHMB, "f")
             return False
-        self.Print("Success!")
-    
-        self.Print("Check if device is still working after Reload NVMe driver ")
+        self.Print("Success!", "p")    
+        self.Print("") 
+        
+        self.Print("2) Check if device is still working after Reload NVMe driver ")
         if not self.ctrl_alive:
             self.Print("Device is missing", "f")
             return False      
-        self.Print("Pass!", "p") 
+        self.Print("Pass!", "p")         
+        self.Print("") 
         
-        return True       
-    
+        self.Print("3) Enable all VF") 
+        if not self.EnableAllVF(): return False        
+        self.Print("") 
+        
+        self.Print("4) Disable all VF") 
+        if not self.DisableAllVF(): return False
+        self.Print("")        
+        
+        return True    
+    def GetVFListCreatedByPF(self):
+    # e.x. mList=[/dev/nvme1n1 , /dev/nvme2n1 ]
+        mList=[]
+        
+        PFPort = self.GetPciePort(self.dev)
+        # if created VF is nvme1n1 nvme2n1 ...
+        if PFPort.find("nvme-subsys")==-1: 
+            PF_PciePort=self.pcie_port
+            # 0000:02:00.0, bus=0000, dev=02, func=00.0
+            # get device bus and device number, remove function number
+            PF_BusDeviceNum = PF_PciePort[:-5]
+            # get nvme list all devices
+            NvmeList=self.GetCurrentNvmeList()
+            for device in NvmeList:
+                Port = self.GetPciePort(device)                            
+                BusDeviceNum = Port[:-5]
+                # if not PF device and (bus and device num) is the same, than it is VF from PF
+                if PF_BusDeviceNum==BusDeviceNum and device!=self.dev:
+                    mList.append(device)           
+        # if created VF is nvme0n1 nvme0n2 ...
+        else:     
+            # get nvme list all devices
+            NvmeList=self.GetCurrentNvmeList()
+            for device in NvmeList:
+                Port = self.GetPciePort(device)  
+                # if Port = nvme-subsys0 and not PF
+                if Port == PFPort and device!=self.dev:
+                    mList.append(device) 
+        return mList
+
+    def GetCurrentNvmeList(self):
+        mStr=self.shell_cmd("nvme list")
+        return re.findall("/dev/nvme\d+n\d+", mStr)
+        
+    # set vf, if success, set AllDevices where AllDevices[0] is PF, others is VF 
+    def SetCurrentNumOfVF(self, value):
+        value=int(value)
+        #path="/sys/class/block/%s/device/device/sriov_numvfs"%self.dev[5:]
+        path="/sys/bus/pci/devices/%s/sriov_numvfs"%self.pcie_port
+        if self.isfileExist(path):
+            if self.shell_cmd("echo %s > %s 2>&1  ; echo $?"%(value, path))!="0" :
+                self.Print("command fail", "f"); return False
+            else:
+                if value!=0: 
+                    # wait for os to create drive        
+                    sleep(2)
+                    # get VF list
+                    self.VFDevices = self.GetVFListCreatedByPF()
+                    # save to AllDevices
+                    self.AllDevices = [self.dev] + self.VFDevices
+                    self.Print("Check if linux os create %s NVMe device under folder /dev/"%value)
+                    self.Print("Created VF drive:")
+                    for Dev in self.VFDevices:
+                        self.Print("        %s"%Dev)
+                    if value==len(self.VFDevices):
+                        self.Print("Pass","p")
+                    else:
+                        self.Print("Fail", "f"); return False
+        else:
+            self.Print("Can't find file: %s"%path, "f"); return False
+        
+        return True 
+        
+    def EnableAllVF(self):
+        # enable all VF        
+        self.Print("") 
+        self.Print("Set all VF online (TotalVFs = %s)"%self.TotalVFs)
+        if not self.SetCurrentNumOfVF(self.TotalVFs):
+            self.Print("Create VF Fail, quit all", "f"); return False  
+        else: 
+            return True
+
+    def DisableAllVF(self):
+        # disable all VF        
+        self.Print("Set all VF offline")
+        if not self.SetCurrentNumOfVF(0):
+            self.Print("Fail, quit all", "f"); return False
+        else: 
+            self.Print("Done", "p")
+            return True 
     
     def __init__(self, argv):
         self.SetDynamicArgs(optionName="l", optionNameFull="loops", helpMsg="number of loops for cases", argType=int)
@@ -46,9 +139,39 @@ class SMI_XT_SRIOV(NVME):
         self.loops = self.GetDynamicArgs(0)
         # set defalut loop =1   
         self.loops=1 if self.loops==None else self.loops        
+        
+        #check OS is isCentOS or Ubuntu
+        self.isCentOS = True if self.shell_cmd("cat /etc/os-release |grep 'CentOS' 2>&1 >/dev/null; echo $?")=="0" else False
+        self.isUbuntu = True if self.shell_cmd("cat /etc/os-release |grep 'Ubuntu' 2>&1 >/dev/null; echo $?")=="0" else False
+        
+        # device lists, first is PF, others is VF, e.g [self.dev ] + self.VFDevices[]
+        self.AllDevices=list(self.dev)
+        self.VFDevices=[]
+        
 
+            
+            
     # define pretest  
-    def PreTest(self):        
+    def PreTest(self):  
+        self.Sysfs_SRIOV_Supported= True if self.GetCurrentNumOfVF()!=None else False
+        if self.Sysfs_SRIOV_Supported:
+            self.Print("Linux's sysfs support SRIOV: Yes")
+        else:
+            self.Print("Linux's sysfs support SRIOV: No, quit all", "w"); return False
+                    
+        self.TotalVFs = int(self.shell_cmd("cat /sys/bus/pci/devices/%s/sriov_totalvfs"%self.pcie_port))               
+        self.Print("Check if TotalVFs of SR-IOV Virtualization Extended Capabilities Register(PCIe Capabilities Registers) is large then 0(SR-IOV supported)") 
+        self.Print("TotalVFs: %s"%self.TotalVFs)
+        if self.TotalVFs>0:
+            self.Print("PCIe device support SR-IOV", "p")
+        else:
+            self.Print("PCIe device do not support SR-IOV, quit all", "w"); return False          
+        
+        self.Print("")
+        nmuvfs = self.GetCurrentNumOfVF()
+        self.Print("Current Num Of VF: %s"%nmuvfs, "p")   
+               
+              
         return True            
 
     # <define sub item scripts>
@@ -74,7 +197,7 @@ class SMI_XT_SRIOV(NVME):
            
         for i in range(self.loops):
             self.Print("")
-            self.Print("-- Current Loop: %s, Total loop: %s"%(i, self.loops))
+            self.Print("-- Current Loop: %s, Total loop: %s"%(i, self.loops), "b")
             
             TargetMHMB=0
             if not self.TestHMB(TargetMHMB): return 1
